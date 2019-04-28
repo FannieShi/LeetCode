27e019eed11c19bc1bf768faaf3ebd1ec71e375e
Fannie Shi <qys9309@qq.com>
2019-04-28 17:11:45 +0800

    LeetCode

---
+++ 9_回文数.js
@@ -0,0 +1,12 @@
+/**
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0){
+        return false
+    }
+    var y = parseInt(x.toString().split('').reverse().join(''))
+    return x === y
+};
