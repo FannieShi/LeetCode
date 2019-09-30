398f52f265c230864508f9186b426ff70b0cb7d5
Fannie Shi <qys9309@qq.com>
2019-09-30 11:49:06 +0800

    LeetCode

---
+++ 1185_ 一周中的第几天.js
@@ -0,0 +1,14 @@
+/**
+ * 给你一个日期，请你设计一个算法来判断它是对应一周中的哪一天。
+ * 输入为三个整数：day、month 和 year，分别表示日、月、年。
+ * 您返回的结果必须是这几个值中的一个 {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}。
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+  const queryObj = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  const date  = new Date(year + '/' + month + '/' + day)
+  return queryObj[date.getDay()]
+};
